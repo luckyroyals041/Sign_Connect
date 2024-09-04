cd0728e3bc1c3e5f85ce6503967995d59d0a351b
luckyroyals041 <115222045+luckyroyals041@users.noreply.github.com>
2024-09-04 08:57:53 +0530

    Add files via upload

---
+++ SignConnect/S2T/step_5_camera.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+import onnxruntime as ort
+
+def center_crop(frame):
+    h, w, _ = frame.shape
+    start = abs(h - w) // 2
+    if h > w:
+        return frame[start: start + w]
+    return frame[:, start: start + h]
+
+def main():
+    # constants
+    index_to_letter = list('ABCDEFGHIKLMNOPQRSTUVWXY')
+    mean = 0.485 * 255.
+    std = 0.229 * 255.
+
+    # create runnable session with exported model
+    ort_session = ort.InferenceSession("signlanguage.onnx")
+
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error: Could not open video stream.")
+        return
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Could not read frame.")
+            break
+
+        # preprocess data
+        cropped_frame = center_crop(frame)
+        gray_frame = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2GRAY)
+        x = cv2.resize(gray_frame, (28, 28))
+        x = (x - mean) / std
+
+        x = x.reshape(1, 1, 28, 28).astype(np.float32)
+        y = ort_session.run(None, {'input': x})[0]
+
+        index = np.argmax(y, axis=1)
+        letter = index_to_letter[int(index)]
+
+        cv2.putText(frame, letter, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), thickness=2)
+        cv2.imshow("Sign Language Translator", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
